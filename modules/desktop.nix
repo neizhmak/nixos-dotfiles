f7595547a907445cf20c15d713fde14bae85cc6b
neizhmak <ytartem0007@gmail.com>
2026-03-10 11:40:43 +0300

    feat: add Orchis-Dark theme with custom tweaks and apply it to GTK 3 and GTK 4, along with the user-themes extension.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -5,24 +5,38 @@
   ...
 }:
 
+let
+  orchis-theme-custom = pkgs.orchis-theme.override {
+    tweaks = [ "solid" "compact" "black" "submenu" ];
+  };
+in
+
 {
   # GNOME и GDM
   services.desktopManager.gnome.enable = true;
   services.displayManager.gdm.enable = true;
 
-  # Фикс диалогов сохранения в Wayland
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
+    GTK_THEME = "Orchis-Dark";
+    # Фикс диалогов сохранения в Wayland
     XDG_DATA_DIRS = [
       "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
       "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
     ];
   };
 
+  # Репликация флага --libadwaita для Orchis-Dark
+  environment.etc."xdg/gtk-4.0/gtk.css".source = "${orchis-theme-custom}/share/themes/Orchis-Dark/gtk-4.0/gtk.css";
+  environment.etc."xdg/gtk-4.0/gtk-dark.css".source = "${orchis-theme-custom}/share/themes/Orchis-Dark/gtk-4.0/gtk-dark.css";
+  environment.etc."xdg/gtk-4.0/assets".source = "${orchis-theme-custom}/share/themes/Orchis-Dark/gtk-4.0/assets";
+
   # Расширения GNOME
   environment.systemPackages = with pkgs; [
     gnomeExtensions.appindicator
     gnomeExtensions.dash-to-dock
+    gnomeExtensions.user-themes
+    orchis-theme-custom
   ];
 
   # Удаление лишнего софта GNOME
